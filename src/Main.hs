a0a51aaaab105170861ee9278037ecbbcc0bc84a
Evan Czaplicki <info@elm-lang.org>
2014-10-10 01:56:40 -0700

    Stop testing Build.Display in Main

--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,5 @@
 module Main where
 
-import Control.Concurrent (forkIO, threadDelay)
-import qualified Control.Concurrent.Chan as Chan
 import GHC.Conc (getNumProcessors, setNumCapabilities)
 
 import qualified Build
@@ -13,33 +11,10 @@ import qualified Elm.Compiler.Module as Module
 
 main :: IO ()
 main =
-    do  numProcessors <- getNumProcessors
-        setNumCapabilities numProcessors
-        updates <- Chan.newChan
-        forkIO $ Display.display updates 0
-        scriptSequence updates updateSequence
+  do  numProcessors <- getNumProcessors
+      setNumCapabilities numProcessors
 
-
-updateSequence =
-    [ Display.Progress 0.1
-    , Display.Progress 0.3
-    , Display.Completion (Module.Name ["Focus"])
-    , Display.Completion (Module.Name ["CommandLine", "Display"])
-    , Display.Progress 0.5
-    , Display.Progress 1
-    , Display.Success
-    ]
-
-
-scriptSequence :: Chan.Chan Display.Update -> [Display.Update] -> IO ()
-scriptSequence _updateChan [] = return ()
-scriptSequence updateChan (update:rest) =
-    do  Chan.writeChan updateChan update
-        threadDelay (1000 * 1000)
-        scriptSequence updateChan rest
-
-{--
-overarchingThing =
-    do  (locations, dependencyNodes) <- Crawl.crawl
-        return ()
---}
+      option <- Options.parse
+      case option of
+        Options.BuildPackage -> return ()
+        Options.BuildFile path -> return ()
